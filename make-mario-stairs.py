99e730b8fa0f428e9281dbe237f0914bcf796b04
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-13 14:26:58 -0800

    added old project

--- make-mario-stairs.py
+++ make-mario-stairs.py
@@ -1,1 +1,15 @@
-# old file add
+#function that takes n and converts it into stair case made of ### to right
+def mario_stairs(n):
+    i = 0
+    s = n
+    while i !=n:
+        i+=1
+        s-=1
+        stairs =' '*s +"#"*i
+        print(stairs)
+    
+
+#prompt user fo input if a number n (int)
+n = int(input("Enter the number of stairs you would like to make for mario:"));
+(mario_stairs(n))
+
